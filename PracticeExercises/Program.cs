d084f5d6e7eb497920b694214aa5653ab23ffec4
Kristan Frost <kristan.frost@gmail.com>
2020-06-26 18:08:40 -0500

    Dictionaries practice complete

---
+++ PracticeExercises/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace PracticeExercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* We're going on an adventure! But before we can go we have to pack our inventory for the journey. 
+
+             To go on our adventure we have 500 Gold, a flint, a rope, 2 healing potions, a dagger, a bedroll, 
+             a loaf of bread, and a tin whistle. 
+
+             1. Create a dictonary called "inventory" that contains each of these items and their values
+             2. Log the contents of your inventory displaying the items and how many of each you have.
+             */
+
+
+            /* As your adventure goes on your party reaches a shop. Now is a good time to restock.
+             Your inventory can hold 10 items (counted by Key not by Value). 
+             
+            The shop currently has an unlimited amount of each item. 
+            Load of Bread - 2 gold
+            Ham Hock - 5 gold
+            Long Sword - 100 gold
+            Short Bow - 75 gold
+            Healing Potion - 25 gold
+            Backpack - 75 gold  <- allows you to carry 10 more items
+            Small Silver Key - 25 gold
+            
+            3. Choose some items to buy, calculate and update the amount of Gold in your Inventory.
+            4. Add those items to your inventory. 
+                - If you purchase more of an item you already have be sure to update that value instead of adding 
+                a new item to your storage. No need to take up a valuable slot.
+
+                - remember it can only hold 10 items so if you bouy the backpack you can add items there
+                .... which means making a new dictionary. 
+
+            5. Log the contents of your inventory (and backpack if you have one) 
+            6. Display a Count of the number of items in your inventory(& backpack).
+            */
+
+
+
+            /*
+            Your party mates HATE your tin whistle, lets just say you're not the greatest musician in the world, ok. 
+            The shop keeper will buy it from you for 10 gold.
+
+            7. Remove the Tin Whistle from your inventory.
+            8. Update your Gold
+            */
+
+
+            /* 
+             Your party comes across a witch in the middle of your path. 
+
+            She says that if any member of your party has a Small Silver Key she'll allow you to pass. 
+            If it doesn't she will destroy you all. 
+
+            9. Search your inventory(& backpack) to see if it contains a Small Silver Key. If it does the witch will tell you "You may pass."
+             If not she will cast "Fireball" at you. 
+             
+             */
+        }
+    }
+}
---
+++ PracticeExercises/Solution/Solution.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dictionaries.Solution
+{
+    class Solution
+    {
+       /* static void Main(string[] args)
+        {
+            // 1.
+            Dictionary<string, int> inventory = new Dictionary<string, int>
+            {
+                {"Gold", 500 },
+                {"Flint", 1 },
+                {"Healing Potion", 2 },
+                {"Tin Whistle", 1 },
+                {"Dagger", 1 },
+                {"Rope", 1 },
+                {"Bedroll", 1 },
+                {"Loaf of Bread", 1 }
+            };
+            //2.
+            foreach (KeyValuePair<string, int> item in inventory)
+            {
+                Console.WriteLine(item.Key + " : " + item.Value);
+            };
+
+            //3. 
+            inventory["Gold"] = 140;
+            // 4.
+            inventory.Add("Small Silver Key", 1);
+            inventory.Add("Long Sword", 1);
+            inventory["Loaf of Bread"] = 6;
+            inventory["Healing Potion"] = 4;
+
+            Dictionary<string, int> backpack = new Dictionary<string, int>
+           {
+               {"Ham Hock", 5 },
+               {"Short Bow", 1 }
+           };
+            // 5. 
+            foreach (KeyValuePair<string, int> item in inventory)
+            {
+                Console.WriteLine(item.Key + " : " + item.Value);
+            };
+
+            Console.WriteLine(inventory.Count);
+
+            foreach (KeyValuePair<string, int> item in backpack)
+            {
+                Console.WriteLine(item.Key + " : " + item.Value);
+            }
+
+            //6.  
+            Console.WriteLine("Inventory Count: " + inventory.Count);
+            Console.WriteLine("Backpack Count: " + backpack.Count);
+
+            //7. 
+            inventory.Remove("Tin Whistle");
+
+            //8. 
+            inventory["Gold"] = 150;
+
+            //9. 
+            if (inventory.ContainsKey("Small Silver Key") || backpack.ContainsKey("Small Silver Key"))
+            {
+                Console.WriteLine("You may pass.");
+            }
+            else
+            {
+                Console.WriteLine("FIREBALL!!!");
+            }
+        }
+    }*/
+}
